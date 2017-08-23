8b5c10d85bd2c02ea7f9380d4d2e46ff2785cf44
Thomas Brunbjerg <thomb@hotmail.dk>
2017-08-23 08:03:04 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var form = document.getElementById("uriForm");
+function addBookmark() {
+    if (form.checkValidity() == true) {
+        var sitename = $("#siteNameInput").val();
+        var siteurl = $("#siteUrlInput").val();
+        var selectedCategory = $('#categorySelectList').find(":selected").text();
+        console.log(sitename, siteurl, selectedCategory);
+
+        if (selectedCategory == "Primary Bookmarks") {
+            $("#primaryBookmarks").append("<a href='" + siteurl + "'><button type='button' class='list-group-item list-group-item-action'>" + sitename + "</button></a>");
+            $("#collapseOne").addClass("show");
+        }
+        else if (selectedCategory == "Other Bookmarks") {
+            $("#otherBookmarks").append("<a href='" + siteurl + "'><button type='button' class='list-group-item list-group-item-action'>" + sitename + "</button></a>");
+            $("#collapseTwo").addClass("show");
+        }
+    }
+}
+
+$(document).ready(function () {
+
+});
